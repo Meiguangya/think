fce8377bc3026db8a8395385caa5f7d12d4a9f33
meiguangya <15871737166@163.com>
2021-02-24 09:11:12 +0800

    一点题目

--- src/mianshiti/RecusionDemo.java
+++ src/mianshiti/RecusionDemo.java
@@ -2,26 +2,63 @@ package mianshiti;
 
 public class RecusionDemo {
 
-    //1.求一个数的阶乘
-    public static int getJC(int n,int z){
+    //1.求一个数的幂
+    public static int getMi(int n,int z){
         if(z==0){
             return 1;
+        }
+        int res = n;
+        while(z>1){
+            res = res * n;
+            z--;
+        }
+        return res;
+    }
+
+
+    //2.求一个数的阶乘
+    public static int getJieCheng(int n) throws Exception {
+        if(n<0){
+            throw new Exception("参数不合法");
+        }
+        if(n==0 || n==1){
+            return 1;
         }else{
-            return doCount(n,z);
+            return n*getJieCheng(n-1);
         }
     }
 
-    public static int doCount(int n,int z){
-        if(z==1){
-            return n;
+    //3.斐波数列
+    //数字规律 1，1，2，3，5，8，13。。。。
+    //给n 求出n位的数
+    public static int getFeiBo(int n){
+        if(n==1 || n==2){
+            return 1;
         }else{
-            return n*doCount(n,z-1);
+            return getFeiBo(n-1)+getFeiBo(n-2);
         }
     }
 
-    public static void main(String[] args) {
-        int res = getJC(5,0);
+    //给定结果数 找到第几位
+    public static int getFeiBoIndex(int n){
+        int temp = 2;
+        int index = 3;
+        while(temp!=n){
+            index++;
+            temp = getFeiBo(index);
+        }
+        return index;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int res = getMi(2,3);
+        int res2 = getJieCheng(0);
+        int res3 = getFeiBo(20);
+        int res4 = getFeiBoIndex(6765);
         System.out.println(res);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
     }
 
 
